341fef67a6e449f4ddc4fbb3d947546cfb62711a
Jishu Developer <143621473+jishusinha@users.noreply.github.com>
2023-12-03 17:20:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     TELEGRAM_API = os.environ["TELEGRAM_API"]
     OWNER = os.environ.get("OWNER")
-    USERNAME = os.environ.get("USERNAME")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    USERNAME = os.environ.get("USERNAME")
     PASSWORD = os.environ.get("PASSWORD")
     DATABASE_URL = os.environ.get("DATABASE_URL")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
-    MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
+    MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
