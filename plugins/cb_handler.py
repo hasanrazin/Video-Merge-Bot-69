8166e7ca2ea5339dfcec5f665017da34fcf6a096
Jishu Developer <143621473+jishusinha@users.noreply.github.com>
2023-12-09 14:02:47 +0530

    Update cb_handler.py

--- plugins/cb_handler.py
+++ plugins/cb_handler.py
@@ -1,6 +1,17 @@
 import asyncio
 import os
+from pyrogram import filters
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+from pyromod.types import ListenerTypes
+from pyromod.listen import Client
 
+from helpers import database
+from helpers.utils import UserSettings
 from bot import (
     LOGGER,
     UPLOAD_AS_DOC,
@@ -10,18 +21,7 @@ from bot import (
     gDict,
     queueDB,
     showQueue,
-    mergeApp
 )
-from helpers import database
-from helpers.utils import UserSettings
-from pyrogram import Client, filters
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-)
-
 from plugins.mergeVideo import mergeNow
 from plugins.mergeVideoAudio import mergeAudio
 from plugins.mergeVideoSub import mergeSub
@@ -56,7 +56,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
             await c.download_media(
                 message=urc, file_name=f"userdata/{cb.from_user.id}/rclone.conf"
             )
-        except Exception as err:
+        except Exception:
             await cb.message.reply_text("Rclone not Found, Unable to upload to drive")
         if os.path.exists(f"userdata/{cb.from_user.id}/rclone.conf") is False:
             await cb.message.delete()
@@ -68,7 +68,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
             return
         UPLOAD_TO_DRIVE.update({f"{cb.from_user.id}": True})
         await cb.message.edit(
-            text="Okay I'll upload to drive\nDo you want to rename? Default file name is **[@Madflix_Bots]_merged.mkv**",
+            text="Okay I'll upload to drive\nDo you want to rename? Default file name is **[@yashoswalyo]_merged.mkv**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -100,7 +100,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
     elif cb.data == "document":
         UPLOAD_AS_DOC.update({f"{cb.from_user.id}": True})
         await cb.message.edit(
-            text="Do you want to rename? Default file name is **[@Madflix_Bots]_merged.mkv**",
+            text="Do you want to rename? Default file name is **[@yashoswalyo]_merged.mkv**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -116,7 +116,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
     elif cb.data == "video":
         UPLOAD_AS_DOC.update({f"{cb.from_user.id}": False})
         await cb.message.edit(
-            text="Do you want to rename? Default file name is **[@Madflix_Bots]_merged.mkv**",
+            text="Do you want to rename? Default file name is **[@yashoswalyo]_merged.mkv**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -131,13 +131,13 @@ async def callback_handler(c: Client, cb: CallbackQuery):
 
     elif cb.data.startswith("rclone_"):
         if "save" in cb.data:
-            fileId = cb.message.reply_to_message.document.file_id
-            LOGGER.info(fileId)
+            message_id = cb.message.reply_to_message.document.file_id
+            LOGGER.info(message_id)
             await c.download_media(
                 message=cb.message.reply_to_message,
                 file_name=f"./userdata/{cb.from_user.id}/rclone.conf",
             )
-            await database.addUserRcloneConfig(cb, fileId)
+            await database.addUserRcloneConfig(cb, message_id)
         else:
             await cb.message.delete()
         return
@@ -146,11 +146,9 @@ async def callback_handler(c: Client, cb: CallbackQuery):
         user = UserSettings(cb.from_user.id, cb.from_user.first_name)
         if "YES" in cb.data:
             await cb.message.edit(
-                "Current filename: **[@Madflix_Bots]_merged.mkv**\n\nSend me new file name without extension: You have 1 minute"
+                "Current filename: **[@yashoswalyo]_merged.mkv**\n\nSend me new file name without extension: You have 1 minute"
             )
-            res: Message = await c.listen(
-                (cb.message.chat.id,None,None), filters=filters.text, timeout=150
-            )
+            res: Message = await c.listen(chat_id=cb.message.chat.id, filters=filters.text, listener_type=ListenerTypes.MESSAGE, timeout=120, user_id=cb.from_user.id)
             if res.text:
                 new_file_name = f"downloads/{str(cb.from_user.id)}/{res.text}.mkv"
                 await res.delete(True)
@@ -160,11 +158,11 @@ async def callback_handler(c: Client, cb: CallbackQuery):
                 await mergeAudio(c, cb, new_file_name)
             elif user.merge_mode == 3:
                 await mergeSub(c, cb, new_file_name)
-
             return
+
         if "NO" in cb.data:
             new_file_name = (
-                f"downloads/{str(cb.from_user.id)}/[@Madflix_Bots]_merged.mkv"
+                f"downloads/{str(cb.from_user.id)}/[@yashoswalyo]_merged.mkv"
             )
             if user.merge_mode == 1:
                 await mergeNow(c, cb, new_file_name)
@@ -210,13 +208,11 @@ async def callback_handler(c: Client, cb: CallbackQuery):
             pass
 
     elif cb.data.startswith("showFileName_"):
-        id = int(cb.data.rsplit("_", 1)[-1])
-        LOGGER.info(
-            queueDB.get(cb.from_user.id)["videos"],
-            queueDB.get(cb.from_user.id)["subtitles"],
-        )
-        sIndex = queueDB.get(cb.from_user.id)["videos"].index(id)
-        m = await c.get_messages(chat_id=cb.message.chat.id, message_ids=id)
+        message_id = int(cb.data.rsplit("_", 1)[-1])
+        LOGGER.info(queueDB.get(cb.from_user.id)["videos"])
+        LOGGER.info(queueDB.get(cb.from_user.id)["subtitles"])
+        sIndex = queueDB.get(cb.from_user.id)["videos"].index(message_id)
+        m = await c.get_messages(chat_id=cb.message.chat.id, message_ids=message_id)
         if queueDB.get(cb.from_user.id)["subtitles"][sIndex] is None:
             try:
                 await cb.message.edit(
@@ -237,7 +233,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
                         ]
                     ),
                 )
-            except:
+            except Exception:
                 await cb.message.edit(
                     text=f"File Name: {m.document.file_name}",
                     reply_markup=InlineKeyboardMarkup(
@@ -279,7 +275,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
                         ]
                     ),
                 )
-            except:
+            except Exception:
                 await cb.message.edit(
                     text=f"File Name: {m.document.file_name}\n\nSubtitles: {s.document.file_name}",
                     reply_markup=InlineKeyboardMarkup(
@@ -316,7 +312,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
             ),
         )
         subs: Message = await c.listen(
-            (cb.message.chat.id,None,None), filters="filters.document", timeout=60
+            chat_id=cb.message.chat.id, filters=filters.document, listener_type=ListenerTypes.MESSAGE, timeout=120, user_id=cb.from_user.id
         )
         if subs is not None:
             media = subs.document or subs.video
@@ -408,18 +404,18 @@ async def callback_handler(c: Client, cb: CallbackQuery):
             cb.message, uid, cb.from_user.first_name, cb.from_user.last_name, user
         )
         return
-    
+
     elif cb.data.startswith('extract'):
         edata = cb.data.split('_')[1]
         media_mid = int(cb.data.split('_')[2])
         try:
             if edata == 'audio':
                 LOGGER.info('audio')
-                await streamsExtractor(c,cb,media_mid,exAudios=True)
+                await streamsExtractor(c, cb, media_mid, exAudios=True)
             elif edata == 'subtitle':
-                await streamsExtractor(c,cb,media_mid,exSubs=True)
+                await streamsExtractor(c, cb, media_mid, exSubs=True)
             elif edata == 'all':
-                await streamsExtractor(c,cb,media_mid,exAudios=True,exSubs=True)
+                await streamsExtractor(c, cb, media_mid, exAudios=True, exSubs=True)
         except Exception as e:
             LOGGER.error(e)
-        
+            
